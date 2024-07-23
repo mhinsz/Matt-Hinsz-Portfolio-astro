9dfb87f7a7e89c253e8c0e2d2a8ad939e44a17d2
mhinsz <mdhh123@gmail.com>
2024-07-23 18:12:28 -0500

    Add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import partytown from '@astrojs/partytown';
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(), 
+    react(),
+    partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ],
   output: "server",
   adapter: netlify()
 });
